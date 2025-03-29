b1f17bb559427f983a98eabb23d8419b44b497c7
guyaldous1 <guyaldous@gmail.com>
2025-03-29 19:46:07 +1100

    refactor with custom class

--- app.py
+++ app.py
@@ -3,14 +3,26 @@ import vlc
 import time  # Add this import for polling
 
 url = 'https://playerservices.streamtheworld.com/api/livestream-redirect/3PBS_FMAAC.m3u8'
-#define VLC instance
-instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
-#Define VLC player
-player=instance.media_player_new()
-#Define VLC media
-media=instance.media_new(url)
-#Set player media
-player.set_media(media)
+
+class VLCPlayer:
+    def __init__(self, url):
+        self.instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
+        self.player = self.instance.media_player_new()
+        self.media = self.instance.media_new(url)
+        self.player.set_media(self.media)
+
+    def play(self):
+        self.player.play()
+        while self.player.get_state() != vlc.State.Playing:
+            time.sleep(0.1)
+
+    def stop(self):
+        self.player.stop()
+        while self.player.get_state() != vlc.State.Stopped:
+            time.sleep(0.1)
+
+# Instantiate VLCPlayer globally
+vlc_player = VLCPlayer('https://playerservices.streamtheworld.com/api/livestream-redirect/3PBS_FMAAC.m3u8')
 
 app = Flask(__name__)
 
@@ -20,22 +32,21 @@ def home():
 
 @app.route('/play')
 def play():
-    player.play()
-    
-    # Wait until the player state is 'Playing'
-    while player.get_state() != vlc.State.Playing:
-        time.sleep(0.1)  # Poll every 100ms
-    
+    vlc_player.play()
     print("VLC instance started playing the stream.")  # Print to console
     return "Playing stream"
 
-
 @app.route('/stop')
 def stop():
-    print(f"Current VLC state before stopping: {player.get_state()}")  # Debugging
-    player.stop()
-    time.sleep(1)  # Give it some time to transition
-    print(f"Current VLC state after stopping: {player.get_state()}")  # Debugging
+    # Log the state of the player before stopping
+    print(f"Player state before stopping: {vlc_player.player.get_state()}")
+    
+    vlc_player.stop()
+    
+    # Log the state of the player after stopping
+    print(f"Player state after stopping: {vlc_player.player.get_state()}")
+    
+    print("VLC instance stopped playing the stream.")  # Print to console
     return "Stopped stream"
 
 if __name__ == '__main__':
