b48563292df61d6d4d50ed74056137dc0f824560
Guy Aldous <guyaldous@gmail.com>
2025-11-04 18:00:11 +1100

    from pi

--- player.py
+++ player.py
@@ -1,10 +1,25 @@
 from flask import Flask
 import vlc
 import time  # Add this import for polling
+import RPi.GPIO as GPIO
 
-# url = 'https://playerservices.streamtheworld.com/api/livestream-redirect/3PBS_FMAAC.m3u8'
-# urlaac = 'https://23193.live.streamtheworld.com/3PBS_FMAACHIGH.aac?dist=3pbswebsite'
-urlnew = 'https://14533.live.streamtheworld.com/3PBS_FMAAC_SC'
+
+urlnew = 'https://playerservices.streamtheworld.com/api/livestream-redirect/3PBS_FMAAC_SC'
+
+### GPIO STUFF ###
+# Constants won't change
+BUTTON_PIN = 16  # The number of the pushbutton pin
+LED_PIN = 18     # The number of the LED pin
+
+# Variables will change
+led_state = GPIO.LOW        # The current state of the LED
+prev_button_state = GPIO.LOW  # The previous state of the button
+button_state = GPIO.LOW  # The current state of the button
+
+# Set up GPIO
+GPIO.setmode(GPIO.BCM)  # Use BCM GPIO numbering
+GPIO.setup(LED_PIN, GPIO.OUT)           # Initialize the LED pin as an output
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Initialize the pushbutton pin as a pull-up input
 
 
 class VLCPlayer:
@@ -27,6 +42,44 @@ class VLCPlayer:
 # Instantiate VLCPlayer globally
 vlc_player = VLCPlayer(urlnew)
 
+## Run the button
+try:
+    while True:
+        # Read the state of the pushbutton value
+        
+        button_state = GPIO.input(BUTTON_PIN)  # Read new state
+
+        # Check if the button state has changed (press or release event)
+        if button_state != prev_button_state:
+            if button_state == GPIO.LOW:  # Button is pressed
+                print("The button is pressed!")
+
+                # Toggle the state of the LED
+                if led_state == GPIO.LOW:
+                    led_state = GPIO.HIGH
+                    vlc_player.play()
+                else:
+                    led_state = GPIO.LOW
+                    vlc_player.stop()
+                # Control LED according to the toggled state
+                GPIO.output(LED_PIN, led_state)
+
+            else:  # Button is released
+                print("The button is released!")
+
+            # Update the previous button state
+            prev_button_state = button_state
+
+        time.sleep(0.01) # Small delay to avoid unnecessary reading
+
+except KeyboardInterrupt:
+    print("\nExiting...")
+    # Clean up GPIO on program exit
+    GPIO.cleanup()
+
+
+
+#Flask Stuff
 app = Flask(__name__)
 
 @app.route('/')
